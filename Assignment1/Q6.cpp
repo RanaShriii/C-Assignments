85ffe32bdeb7e32411daa1ee4cd15857e6c37b79
RanaShriii <145842862+RanaShriii@users.noreply.github.com>
2023-09-23 23:21:58 +0530

    Add files via upload

---
+++ Assignment1/Q6.cpp
@@ -0,0 +1,12 @@
+//Ques: WAP to find the circumference of a circle
+//with radius 10 in C++.
+
+#include <iostream>
+using namespace std;
+int main(){
+          float r = 10;
+          float circumference = 2*3.14*r;
+          cout << circumference;
+          return 0;
+
+}
